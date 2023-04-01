2eb4ed04d6ae22afdab7cfb0a054a13c5d64587c
Hoang Tran <tdll.trdphoang@gmail.com>
2023-04-01 00:45:30 -0700

    Fixed font size for headers

--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,7 @@ const styles = {
 		"text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
 	sectionHeadText:
-		"text-[#D7CEE4] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+		"text-[#D7CEE4] font-black md:text-[70px] sm:text-[60px] xs:text-[60px] text-[40px]",
 	sectionSubText:
 		"sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
