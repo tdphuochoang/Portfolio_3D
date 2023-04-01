04ac84c2db70879c46a9407b7986da7a1a390eae
Hoang Tran <tdll.trdphoang@gmail.com>
2023-03-31 17:48:03 -0700

    Fixed import & export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,4 @@
-import {
-	EarthCanvas,
-	BallCanvas,
-	ComputersCanvas,
-	StarsCanvas,
-} from "./canvas";
+import { BallCanvas, ComputersCanvas } from "./canvas";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -20,8 +15,6 @@ export {
 	Experience,
 	Works,
 	Contact,
-	EarthCanvas,
 	BallCanvas,
 	ComputersCanvas,
-	StarsCanvas,
 };
